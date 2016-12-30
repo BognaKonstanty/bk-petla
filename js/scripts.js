9ee1d0f096097926c1236ff8f586542412101564
Bogna Konstanty <rosiakb@gmail.com>
2016-12-30 11:16:12 +0100

    add new file

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+
+function rysujChoinkę(n) {
+	for (i = 1; i <= n; i++ ) {
+		var star = '';
+		for (j = 1; j <= i*2-1; j++) {	  
+     		var star = star + '*';
+      	}
+    	console.log(star)
+  	}
+};
+rysujChoinkę(5);
+
